3cba7ac4fceb3a6c7368054db8e158acd37e5b7b
Mike746 <55503808+Mike746@users.noreply.github.com>
2019-10-04 22:53:10 -0500

    Add files via upload

---
+++ practica 2.c
@@ -0,0 +1,47 @@
+/* practica 2
+nombre:miguel angel rodriguez ramirez, grupo 1AV6*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+int main (void) {
+char repeticion;
+float a;
+float b;
+float c;
+int opt;
+do {
+printf ("eligue una operacion \n 1.-suma \n 2.-resta \n 3.-miltuplicacion \n 4.-division \n");
+scanf ("%d", &opt);
+printf ("ingresa el primer valor \n");
+scanf ("%f", &a);
+printf ("ingresa el segundo valor \n");
+scanf("%f",&b);
+switch (opt) {
+case 1:
+c=a+b;
+break;
+case 2:
+c=a-b;
+break;
+case 3:
+c=a*b;
+break;
+case 4:
+while (b==0){
+printf ("eligue un valor diferente de 0 para b \n");
+scanf ("%f", &b);
+}
+c=a/b;
+break;
+default:
+    printf ("opcion invalida");
+}
+printf ("tu resultado es %f \n" , c);
+printf ("quieres hacer otra operacion S N: \n");
+repeticion =getch();
+
+}
+while (repeticion=='S' || repeticion=='s');
+}
+
